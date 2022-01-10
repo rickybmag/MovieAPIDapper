ab5742ed589411902e546776cb6bc23111d9446d
Rick Magdaleno <rickmagdaleno@rocketmortgage.com>
2022-01-10 07:08:25 -0700

    Upload of API. Couldn't get title methods to work. But random and random genre worked

---
+++ MovieAPIDapper/Controllers/MovieController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAPIDapper.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        MovieDAL md = new MovieDAL();
+
+        [HttpGet]
+        public List<Movie> GetMovies()
+        {
+            return md.GetAllMovies();
+        }
+
+        [HttpGet("GetGenres/{genre}")]
+        public List<Movie> GetByGenre(string genre)
+        {
+            List<Movie> movies = md.GetAllMovies();
+            List<Movie> filteredMovies = new List<Movie>();
+            foreach (var movie in movies)
+            {
+                if (movie.Genre.Trim().ToLower() == genre.Trim().ToLower())
+                {
+                    filteredMovies.Add(movie);
+                }
+            }
+            return filteredMovies;
+        }
+
+        [HttpGet("Random")]
+        public Movie GetRandomMovie()
+        {
+            return md.GetRandomMovie();
+        }
+
+        [HttpGet("RandomGenre/{genre}")]
+        public Movie GetRandomByGenre(string genre)
+        {
+            return md.GetRandomGenre(genre);
+        }
+
+        [HttpGet("Randomtitle/{title}")]
+        public Movie GetRandomTitle(string title)
+        {
+            return md.GetRandomTitle(title);
+        }
+    }    
+}
---
+++ MovieAPIDapper/MovieDAL.cs
@@ -0,0 +1,117 @@
+﻿using Dapper;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAPIDapper
+{
+    public class MovieDAL
+    {
+        public List<Movie> GetAllMovies()
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                var sql = "select * from movies";
+                connect.Open();
+                List<Movie> movie = connect.Query<Movie>(sql).ToList();
+                connect.Close();
+                return movie;
+            }
+        }
+
+        public List<string> GetGenres()
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                List<string> genre = new List<string>();
+                string sql = $"select * from movies where genre = '{genre}'";
+                connect.Open();
+                List<Movie> movies = connect.Query<Movie>(sql).ToList();
+                connect.Close();
+                foreach (var movie in movies)
+                {
+                    genre.Add(movie.Genre);
+                }
+                return genre;
+            }
+        }
+
+        public Movie GetTitle(string title)
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                List<string> genre = new List<string>();
+                string sql = $"select * from movies where title = '{title}'";
+                connect.Open();
+                Movie movie = connect.Query<Movie>(sql).ToList().First();
+                connect.Close();                
+                return movie;
+            }
+        }
+
+        public Movie GetRandomMovie()
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                var sql = "select * from movies";
+                connect.Open();
+                List<Movie> movies = connect.Query<Movie>(sql).ToList();
+                
+                Random r = new Random();
+                int rNum = r.Next(0, movies.Count);
+                connect.Close();
+
+                return movies[rNum];
+            }
+        }
+
+        public Movie GetRandomGenre(string genre)
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = $"select * from movies where genre = '{genre}'";
+                try
+                {
+                    connect.Open();
+                    List<Movie> movies = connect.Query<Movie>(sql).ToList();
+                    Random r = new Random();
+                    int rNum = r.Next(0, movies.Count);
+                    connect.Close();
+                    return movies[rNum];
+                }
+                catch (InvalidOperationException)
+                {
+                    Movie error = new Movie();
+                    error.Title = $"Coudldn't find genre \"{genre}\". Try again'";
+                    return error;
+                }
+            }
+        }
+
+        public Movie GetRandomTitle(string title)
+        {
+            using (var connect = new MySqlConnection(Secret.Connection))
+            {
+                string sql = $"select * from movies where title = '{title}'";
+                try
+                {
+                    connect.Open();
+                    List<Movie> movies = connect.Query<Movie>(sql).ToList();
+                    Random r = new Random();
+                    int rNum = r.Next(0, movies.Count);
+                    connect.Close();
+                    return movies[rNum];
+                }
+                catch (InvalidOperationException)
+                {
+                    Movie error = new Movie();
+                    error.Title = $"Coudldn't find \"{title}\" as a movie. Try again'";
+                    return error;
+                }
+            }
+        }
+
+    }
+}
